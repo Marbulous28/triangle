5e934d51df47eff3f8ee8809d48c3b34193b2cef
Peter Beach and Ryan Harvey <peter@>
2016-03-24 15:10:09 -0700

    Triangle Time

--- js/my.js
+++ js/my.js
@@ -1,10 +1,29 @@
-$(document).ready(function() {
-  $("form#information").submit(function(event) {
+$(document).ready(function(){
+  $("form#triangle").submit(function(event){
     event.preventDefault();
-    var nameInput = $("#name").val();
-    var addressInput = $("#address").val();
-    $(".outputAddress").text(addressInput);
-    $("#outputName").text(nameInput);
-    $("#receipt").show();
+    var a = parseInt($('#side1').val());
+    var b = parseInt($('#side2').val());
+    var c = parseInt($('#side3').val());
+
+    if (a === undefined || b === undefined || c === undefined) {
+      alert('YA DONE FUCKED IT ALL UP');
+
+    } else {
+      if (((c + a) <= b) || ((a + b) <= c) || ((c + b) <= a)){
+       alert('This is not a triangle, triangle again!')
+      }
+
+      else if (a === b && b === c) {
+        alert('This triangle is equilateral');
+       }
+
+      else if (((a === b) && (b !== c)) || ((a === c) && (c !== b)) || ((c === b) && (a !== c))) {
+        alert('This triangle is isosceles');
+       }
+
+      else if (a !== b && b !== c){
+        alert('This is a scalene triangle');
+      }
+    }
   });
 });
